74829b2c1ba1b54a2252fdd856e5be5a646a1b18
SXongin <e2581108847@outlook.com>
2018-02-09 22:32:21 +0800

    complete digit_counting.cpp.

---
+++ chapter03/digit_counting.cpp
@@ -0,0 +1,25 @@
+/*UVa 1225
+* 把前n（n <= 10000)个整数顺次写在一起：123456789101112...数一数 0~9 各出现了多少次
+* （输出10个整数，分别时0，1，2，...，9出现的次数）。
+*/
+
+#include <cstdio>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    int results[10] = {0};
+    for(int i = 1; i <= n; ++i){
+        int m;
+        int x = i;
+        while(x>0){
+            m = x%10;
+            x = x/10;
+            ++results[m];
+        }
+    }
+    for(int i = 0; i < 9; ++i){
+        printf("%d ", results[i]);
+    }
+    printf("%d\n", results[9]);
+}
