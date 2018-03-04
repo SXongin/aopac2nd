8899ad34c2e143b4f401977a7f99f399a5aab8f1
SXongin <e2581108847@outlook.com>
2018-03-04 15:19:19 +0800

    digit_counting.cpp: add input and output txt and can solve T case.

--- chapter03/digit_counting.cpp
+++ chapter03/digit_counting.cpp
@@ -6,20 +6,29 @@
 #include <cstdio>
 
 int main(void){
-    int n;
-    scanf("%d", &n);
-    int results[10] = {0};
-    for(int i = 1; i <= n; ++i){
-        int m;
-        int x = i;
-        while(x>0){
-            m = x%10;
-            x = x/10;
-            ++results[m];
+#ifndef ONLINE_JUDGE
+    freopen("digit_counting_input.txt", "r", stdin);
+    freopen("digit_counting_output.txt", "w", stdout);
+#endif
+    int T;
+    scanf("%d", &T);
+    while(T>0){
+        --T;
+        int n;
+        scanf("%d", &n);
+        int results[10] = {0};
+        for(int i = 1; i <= n; ++i){
+            int m;
+            int x = i;
+            while(x>0){
+                m = x%10;
+                x = x/10;
+                ++results[m];
+            }
         }
+        for(int i = 0; i < 9; ++i){
+            printf("%d ", results[i]);
+        }
+        printf("%d\n", results[9]);
     }
-    for(int i = 0; i < 9; ++i){
-        printf("%d ", results[i]);
-    }
-    printf("%d\n", results[9]);
 }
