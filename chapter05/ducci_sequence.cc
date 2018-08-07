64e0f5fdadb898af0ba8a3db580cad161d4a63e4
SXongin <e2581108847@outlook.com>
2018-08-07 11:57:16 +0800

    ducci sequence UVa 1594: use unordered_set and the hash function for std::vector.

--- chapter05/ducci_sequence.cc
+++ chapter05/ducci_sequence.cc
@@ -3,11 +3,37 @@
 
 #include <array>
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
+//using boost::hash_combine
+template <class T>
+inline void hash_combine(std::size_t& seed, T const& v)
+{
+    seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+namespace std
+{
+    template<typename T>
+    struct hash<vector<T>>
+    {
+        typedef vector<T> argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& in) const
+        {
+            size_t size = in.size();
+            size_t seed = 0;
+            for (size_t i = 0; i < size; i++)
+                //Combine the hash of the current vector with the hashes of the previous ones
+                hash_combine(seed, in[i]);
+            return seed;
+        }
+    };
+}
+
+
 const int kMax = 2010;
-std::set<std::vector<int> > appear;
+std::unordered_set<std::vector<int> > appear;
 
 int main(void)
 {
